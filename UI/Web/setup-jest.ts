cc36bff96efd3b3d4eb620370f9ad327e79dfdde
Hobogrammer <lordsilent@gmail.com>
2023-10-19 19:47:16 -0700

    Working on getting the object under test properly instantiated
    - Might have fucked up the merge here

--- UI/Web/setup-jest.ts
+++ UI/Web/setup-jest.ts
@@ -1,1 +1,5 @@
 import 'jest-preset-angular/setup-jest';
+import { ngMocks } from 'ng-mocks';
+
+// enable automatic spying for all mocked objects
+ngMocks.autoSpy('jest');
--- UI/Web/src/app/book-reader/_components/book-reader/book-reader.component.spec.ts
+++ UI/Web/src/app/book-reader/_components/book-reader/book-reader.component.spec.ts
@@ -1,20 +1,54 @@
 import { BookReaderComponent } from './book-reader.component';
-import { TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AccountService } from 'src/app/_services/account.service';
+import { BookService } from '../../_services/book.service';
+import { NavService } from 'src/app/_services/nav.service';
+import { SeriesService } from 'src/app/_services/series.service';
+import { ThemeService } from 'src/app/_services/theme.service';
+import { ScrollService } from 'src/app/_services/scroll.service';
+import { LibraryService } from 'src/app/_services/library.service';
+import { MemberService } from 'src/app/_services/member.service';
+import { UtilityService } from 'src/app/shared/_services/utility.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MockInstance, MockProvider } from 'ng-mocks';
+import {FilterUtilitiesService} from 'src/app/shared/_services/filter-utilities.service';
+import { EMPTY } from 'rxjs';
 
 describe('BookReaderComponent', () => {
+  let component: BookReaderComponent;
+  let fixture: ComponentFixture<BookReaderComponent>;
 
   beforeEach(async () => {
-    TestBed.configureTestingModule({
-        declarations: [ BookReaderComponent ],
-    });
+    await TestBed.configureTestingModule({
+      imports: [BookReaderComponent, HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        MockProvider(AccountService),
+        MockProvider(SeriesService),
+        MockProvider(NavService),
+        MockProvider(ToastrService),
+        MockProvider(MemberService),
+        MockProvider(BookService),
+        MockProvider(ScrollService),
+        MockProvider(UtilityService),
+        MockProvider(LibraryService),
+        MockProvider(ThemeService)
+      ]
+    }).compileComponents();
+  });
 
-    let fixture = TestBed.createComponent(BookReaderComponent);
-    let testReaderComponent = fixture.componentInstance;
+  beforeAll(() => MockInstance(AccountService, 'currentUser$', EMPTY));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookReaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
 
   it('should always progress linearly, not skipping pages in two column mode', () => {
-
+    expect(component).toBeTruthy();
   });
 });
