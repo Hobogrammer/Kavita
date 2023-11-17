d8cd1aed5c95a82187dc380994d36e7a840bb3b2
Hobogrammer <lordsilent@gmail.com>
2023-11-16 19:53:08 -0800

    Change approach a little bit

---
+++ API/Constants/BookReaderActions.cs
@@ -0,0 +1,11 @@
+namespace API.Constants;
+
+public static class BookReaderActions
+{
+    public const string NextPage = "NextPage";
+    public const string PreviousPage = "PrevPage";
+    public const string Close = "Forward";
+    public const string ToggleMenu = "Forward";
+    public const string GoToPage = "GoToPage";
+    public const string Fullscreen = "Fullscreen";
+}
---
+++ API/Entities/Enums/Shortcuts/BookReaderAction.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.Json;
+using API.Constants;
+
+namespace API.Entities;
+
+public class BookReaderKeyActionMap
+{
+    // Initialize the hashset with the default key shortcuts
+    public HashSet<string, string> set = new HashSet<string, string>()
+    {
+        { "ArrowRight" , BookReaderActions.Forward},
+        { "ArrowLeft" , BookReaderActions.Backward},
+        { "Escape" , BookReaderActions.Close},
+        { " " , BookReaderActions.ToggleMenu},
+        { "g" , BookReaderActions.GoToPage},
+        { "f" , BookReaderActions.Fullscreen}
+    };
+
+    public override string ToString() => JsonSerializer.Serialize(set);
+    
+    public void UpdateShortcut(string KeyCode, string Action)
+    {
+        if (set.TryGetValue(KeyCode)) {
+            set.Remove(KeyCode);
+            set.Add(KeyValuePair<KeyCode, Action>);
+        } else {
+            set.Add(KeyValuePair<KeyCode, Action>);
+        }
+    }
+}
