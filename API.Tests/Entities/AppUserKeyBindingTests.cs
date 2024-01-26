67fb335e4d959648626fd48d334621e1613be85a
Hobogrammer <lordsilent@gmail.com>
2024-01-25 17:48:26 -0800

    Add entity test

--- API.Tests/Entities/AppUserKeyBindingTests.cs
+++ API.Tests/Entities/AppUserKeyBindingTests.cs
@@ -1,14 +1,20 @@
+using System.Collections.Immutable;
+using API.Entities.Enums;
 using Xunit;
 
 namespace API.Tests.Entities;
 
 public class AppUserKeybBindingTests
 {
+    private ImmutableList<ReaderAction> ExpectedBookAndMangaActions = ImmutableList.Create(new ReaderAction[] { ReaderAction.NextPage, ReaderAction.PreviousPage, ReaderAction.Close,ReaderAction.ToggleMenu, ReaderAction.GoToPage, ReaderAction.FullScreen });
+
     [Theory]
-    // Manga [InlineData()]
-    // Book
-    // PDF
-    public void getActionListForType_ShouldReturnExpectedActionList(ReaderType readerType)
+    [InlineData(ReaderType.Manga, ExpectedBookAndMangaActions)]
+    [InlineData(ReaderType.Book, ExpectedBookAndMangaActions)]
+    [InlineData(ReaderType.Pdf, ImmutableList.Create(new ReaderAction[] { ReaderAction.Close }))]
+    public void getActionListForType_ShouldReturnExpectedActionList(ReaderType readerType, ImmutableList expectedActionList)
     {
+        keyBinding = new AppUserKeyBinding(readerType);
+        Assert.Equal(keyBinding.ApplicableActionList, expectedActionList);
     }
 }
