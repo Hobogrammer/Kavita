72023ba9db641a81299ef600832d086ec467cf13
Hobogrammer <lordsilent@gmail.com>
2024-01-30 17:18:44 -0800

    Add KeyBindings validators

--- API/Validators/KeyBindingTypeActionValidationAttribute.cs
+++ API/Validators/KeyBindingTypeActionValidationAttribute.cs
@@ -1,7 +1,49 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using API.Constants;
+using API.Entities.Enums;
 
 namespace API.Validators;
 
-public class KeyBindingTypeActionAttribute : ValidationAttribute
+// Validate that actions used are allowed for given ReaderType
+public class ValidReaderActions: ValidationAttribute
 {
+    public Dictionary<ReaderAction, string> Bindings { get; }
+
+    public ValidReaderActions(Dictionary<ReaderAction, string> bindings)
+    {
+        Bindings = bindings;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var actions = Bindings.Keys;
+        var keyBinding = (AppUserKeyBinding)validationContext.ObjectInstance;
+        var readerType = keyBinding.Type;
+        ImmutableSet<ReaderAction> validActions;
+
+        switch(readerType)
+        {
+            case ReaderType.Book:
+                validActions = ReaderTypeActionSet.BookActions;
+                break;
+            case ReaderType.Manga:
+                validActions = ReaderTypeActionSet.MangaActions;
+                break;
+            case ReaderType.Pdf:
+                validActions = ReaderTypeActionSet.PdfActions;
+                break;
+        }
+
+        foreach(ReaderAction action in actions)
+        {
+            if (!validActions.Contains(action))
+            {
+
+                return new ValidationResult($"{action} is not allowed for ReaderType {readerType}");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
 }
--- API/Validators/KeyBindingUniqueKeyValidationAttribute.cs
+++ API/Validators/KeyBindingUniqueKeyValidationAttribute.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using API.Entities.Enums;
 
 namespace API.Validators;
 
-public class KeyBindingUniqueKeyAttribute : ValidationAttribute
+// Validate that all keys tied to actions are unique and non-repeating
+public class UniqueKeysAttribute : ValidationAttribute
 {
+    public const string ErrorMessage = "Keys cannot be assigned to multiple actions.";
+    public Dictionary<ReaderAction, string> Bindings { get; }
+
+    public UniqueKeysAttribute(Dictionary<ReaderAction, string> bindings)
+    {
+        Bindings = bindings;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (!Bindings.Values.Distinct().Count() == Bindings.Count)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
+    }
 }
