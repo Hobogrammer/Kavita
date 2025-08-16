747704993f3e843e321bec0133c2781157d47551
Hobogrammer <andrepsmithjr@gmail.com>
2025-08-16 01:14:32 -0700

    Add more tables to the database. Add a few more tests to start off with

--- UI/Web/src/app/shared/_services/local-repository.service.ts
+++ UI/Web/src/app/shared/_services/local-repository.service.ts
@@ -2,6 +2,9 @@
 import LocalRepository from '../local-object-store/local-repository';
 import { Library } from "src/app/_models/library/library";
 import {Local} from "d3";
+import { Series } from "src/app/_models/series";
+import { SeriesDetail } from "src/app/_models/series-detail/series-detail";
+import { SeriesMetadata } from "src/app/_models/metadata/series-metadata";
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,19 @@ export class LocalRepositoryService {
     return await this.db.libraries.add(value);
   }
 
+  public async addSeries(value: Series) {
+
+  }
+
+  public async addSeriesDetail(value: SeriesDetail) {
+
+  }
+
+  public async addSeriesMetadata(value: SeriesMetadata) {
+
+  }
   public async getAllLibraries(){
     return this.db.libraries.toCollection();
   }
+
 }
--- UI/Web/src/app/shared/_services/opfs.service.ts
+++ UI/Web/src/app/shared/_services/opfs.service.ts
@@ -1,4 +1,4 @@
-﻿import { OnInit } from "@angular/core";
+﻿import {Injectable } from "@angular/core";
 
 @Injectable({
   providedIn: 'root',
