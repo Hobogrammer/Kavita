588a8496637e2cb3919f22005b1cd6d9844ee0fd
Hobogrammer <andrepsmithjr@gmail.com>
2025-08-28 18:03:44 -0700

    Add playwright. Move some things around to better dependency inject

--- UI/Web/src/app/workers/cache.worker.ts
+++ UI/Web/src/app/workers/cache.worker.ts
@@ -1,13 +1,37 @@
 /// <reference lib="webworker" />
 
-const FOLDER_NAME = "kavita";
+/*
+import { Library } from "./_models/library/library";
+import { Series } from "./_models/series";
+*/
 
-addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
-  postMessage(response);
+//const FOLDER_NAME = "kavita";
+
+addEventListener('message', ({ data }: MessageEvent) => {
+  const msg = data.message;
+  postMessage(handleMessage(msg))
 });
 
-class Opfs {
+async function handleMessage(msg:any) { // adjust this :any later
+//  const series:Series = msg.series; For later
+//  const library:Library = msg.library; For later
+  const file:File = msg.file;
+
+  const opfsRoot = await navigator.storage.getDirectory();
+
+  const fileHandle: FileSystemFileHandle = await opfsRoot.getFileHandle(file.name, {create: true});
+  const syncAccessHandle = await fileHandle.createSyncAccessHandle();
+  syncAccessHandle.write(await file.arrayBuffer(), {at: 0})
+  syncAccessHandle.flush();
+  syncAccessHandle.close();
+  // return filepath for saved file?
+}
+
+class CacheWorker extends Worker {
+
+}
+/*
+class CacheWorker {
   private opfsRoot!: FileSystemDirectoryHandle;
   private kavitaRoot!: FileSystemDirectoryHandle;
 
@@ -18,14 +42,6 @@ class Opfs {
     this.opfsRoot.getDirectoryHandle(FOLDER_NAME, {create: true}).then(result => {
       this.kavitaRoot = result;
     });
-  }
-
-  public async saveFileSync(filename: string, extension: string, file: Blob) {
-    const fileHandle: FileSystemFileHandle = await this.opfsRoot.getFileHandle(filename + '.' + extension, {create: true});
-    const syncAccessHandle = await fileHandle.createSyncAccessHandle();
-    syncAccessHandle.write(await file.arrayBuffer(), {at: 0})
-    syncAccessHandle.flush();
-    syncAccessHandle.close();
   }
 
   public async getFile(filename: string): Promise<Blob> {
@@ -40,5 +56,6 @@ class Opfs {
     await directoryHandle.removeEntry(filename);
   }
 }
+*/
 
 
