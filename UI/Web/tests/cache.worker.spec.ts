7ecce9754f3e2c31286401c6a9a4e22dbd4f236e
Hobogrammer <andrepsmithjr@gmail.com>
2025-09-18 16:44:46 -0700

    Remove wrightplay. Attempt first e2e test. Still need to figure out best practices on db seeding

--- UI/Web/tests/cache.worker.spec.ts
+++ UI/Web/tests/cache.worker.spec.ts
@@ -1,2 +1,17 @@
-﻿import {onInit} from 'wrightplay';
+﻿import {test, expect} from '@playwright/test';
 
+// Pre-setup required
+// 1. Create or seed accounts
+// 2. Login
+// 3. Create or seed content
+test('should be created', async ({page}) => {
+  await page.goto('/login' );
+
+  await expect(page.getByLabel('Username')).toBeVisible();
+  await page.getByPlaceholder("Username").fill('admin');
+  await page.getByPlaceholder("Password").fill('adminadmin');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.waitForURL('**/home');
+
+  expect(page.workers().length).toBe(1);
+});
