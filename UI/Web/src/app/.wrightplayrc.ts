e82a3917c231b45035009bfca13dc5b3117da706
Hobogrammer <andrepsmithjr@gmail.com>
2025-09-18 16:44:46 -0700

    Remove wrightplay. Attempt first e2e test. Still need to figure out best practices on db seeding

--- UI/Web/src/app/.wrightplayrc.ts
+++
@@ -1,9 +0,0 @@
-﻿import type {ConfigOptions} from 'wrightplay/node';
-
-
-export const config: ConfigOptions = {
-  entryPoints: {
-    worker: "./workers/cache.worker.ts"
-  },
-  tests: './test/**.test.*',
-}
