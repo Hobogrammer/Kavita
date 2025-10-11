92ff81ac6ac9a62c42bd72137e780a7b5fd6801e
Hobogrammer <andrepsmithjr@gmail.com>
2025-10-10 19:30:15 -0700

    Refactor WIP
    - Started writing specs for login
    - Created test-user-builder
      - Still needs to be tested. Or genericized

---
+++ UI/Web/utils/playwright-utils.ts
@@ -0,0 +1,28 @@
+﻿import {Page} from '@playwright/test';
+
+export async function setRoute(page: Page, url: string, response: any, options?: any) {
+  await page.route(url, async route => {
+    await route.fulfill({
+      contentType: options?.contentType || "application/json",
+      status: options?.status || 200,
+      body: JSON.stringify(response)
+    });
+  });
+}
+
+export async function setWebSocketRoute(page: Page, url: string, user: any) {
+  await page.routeWebSocket('ws://'+ url, ws => {
+    console.log("Serving on WebSocket");
+    ws.onMessage(message => {
+      switch (message) {
+        case "{'protocol':'json','version':1}":
+          ws.send(JSON.stringify({}));
+          ws.send(JSON.stringify({"type":1,"target":"OnlineUsers","arguments":[[user.username]]}));
+          break;
+        default:
+          ws.send("{}");
+          break;
+      }
+    });
+  });
+}
---
+++ UI/Web/utils/test-user-builder.ts
@@ -0,0 +1,71 @@
+import { AgeRestriction } from "src/app/_models/metadata/age-restriction";
+import { Preferences } from "src/app/_models/preferences/preferences";
+import { User } from "src/app/_models/user";
+
+export default class TestUserBuilder {
+  private user: Partial<User> = {
+    refreshToken: 'aTotallyRealRefreshToken',
+    apiKey: '12345',
+    hasRunScrobbleEventGeneration: false,
+    scrobbleEventGenerationRan: ''
+  }
+
+  withAgeRestriction(ageRestriction: AgeRestriction): this {
+    this.user.ageRestriction = ageRestriction;
+    return this;
+  }
+  withAPIKey(apiKey: string): this {
+    this.user.apiKey = apiKey;
+    return this;
+  }
+
+  withEmail(email: string): this {
+    this.user.email = email;
+    return this;
+  }
+
+  withPreferences(preferences: Preferences): this {
+    this.user.preferences = preferences;
+    return this;
+  }
+
+  withRefreshToken(refreshToken: string): this {
+    this.user.refreshToken = refreshToken;
+    return this;
+  }
+
+  withRoles(roles: string[]): this {
+    this.user.roles = roles;
+    return this;
+  }
+
+  withUsername(username: string): this {
+    this.user.username = username;
+    return this;
+  }
+
+  build(): User {
+    this.user.token = this.createJwt();
+    return {...this.user} as User;
+  }
+
+  // Create a mostly complete JWToken
+  // Final section (signature) is not generated
+  private createJwt(): string {
+    const currentTime = new Date();
+    const tenDaysInSeconds = 24 * 60 * 60 * 10;
+    const jwtHeader = { alg: "HS256", typ: "JWT" };
+    // @ts-ignore
+    const jwtPayload = {
+      name: this.user.username,
+      nameId: 1,
+      role: this.user.roles,
+      nbf: currentTime.getUTCSeconds(), // time before jwt must not be accepted
+      exp: currentTime.getUTCSeconds() + tenDaysInSeconds, // expiration time on or after which jwt should not be accepted
+      iat: currentTime.getUTCSeconds() // time at which token was issued
+    };
+
+    // Convert stringified JSON to base64 and join
+    return [btoa(JSON.stringify(jwtHeader)), btoa(JSON.stringify(jwtPayload))].join('.');
+  }
+}
