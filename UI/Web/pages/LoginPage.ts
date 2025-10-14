dbf765f6699ddd8b6c87a1a9d4d57d4b0b527146
Hobogrammer <andrepsmithjr@gmail.com>
2025-10-14 15:38:54 -0700

    Cover two other login test cases
    - Logging in when there isn't a pre-exisiting user
    - Failure to login
    - Also moved locale response to its own file to tidy things up

--- UI/Web/pages/LoginPage.ts
+++ UI/Web/pages/LoginPage.ts
@@ -1,12 +1,38 @@
 ﻿import { Page } from "@playwright/test";
+import { SiteTheme } from "src/app/_models/preferences/site-theme";
+import { environment } from "src/environments/environment";
+import { setRoute } from "utils/playwright-utils";
+import * as localeResponse from "tests/data/locale.json"
 
 export class LoginPage {
+  private siteTheme: SiteTheme = {
+    id:1,
+    name: "Dark",
+    normalizedName: "dark",
+    fileName: "dark.scss",
+    isDefault: true,
+    provider: 1,
+    previewUrls: [""],
+    description:"Default theme shipped with Kavita",
+    author: "",
+    compatibleVersion: null,
+    selector: "bg-dark",
+    filePath: "assets/css/dark.scss",
+  } as SiteTheme;
+
   constructor(private page: Page) {}
 
   async login(username: string, password: string) {
+    // Set routes required to load login page
+    await setRoute(this.page, environment.apiUrl + 'admin/exists', true);
+    await setRoute(this.page, environment.apiUrl + 'theme', [this.siteTheme]);
+    await setRoute(this.page, environment.apiUrl + 'locale', localeResponse);
+
+    // Navigate to login page
     await this.page.goto('/login');
     await this.page.waitForLoadState();
 
+    // Fill login form
     await this.page.getByPlaceholder("Username").fill(username);
     await this.page.getByPlaceholder("Password").fill(password);
     await this.page.getByRole('button', { name: 'Sign in' }).click();
