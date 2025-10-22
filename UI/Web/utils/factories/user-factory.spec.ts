8be138f58b2c4337cd5c1f3ad15617c93ac5b7d3
Hobogrammer <andrepsmithjr@gmail.com>
2025-10-22 15:12:15 -0700

    WIP. Tests for UserFactory

---
+++ UI/Web/utils/factories/user-factory.spec.ts
@@ -0,0 +1,56 @@
+﻿import {UserFactory} from "./user-factory";
+import {User} from "../../src/app/_models/user";
+import {AgeRestriction} from "../../src/app/_models/metadata/age-restriction";
+import {Preferences} from "../../src/app/_models/preferences/preferences";
+import {defaultSiteTheme} from "../playwright-utils";
+import {PageLayoutMode} from "../../src/app/_models/page-layout-mode";
+import {AgeRating} from "../../src/app/_models/metadata/age-rating";
+describe('UserFactory', () => {
+  const expectedPref = {
+    theme: defaultSiteTheme,
+    globalPageLayoutMode: PageLayoutMode.List,
+    blurUnreadSummaries: false,
+    promptForDownloadSize: false,
+    noTransitions: false,
+    collapseSeriesRelationships: false,
+    shareReviews: false,
+    locale: "en",
+    aniListScrobblingEnabled: false,
+    wantToReadSync: false
+  } as Preferences;
+
+  const expectedAgeRestriction = {
+    ageRating: AgeRating.NotApplicable,
+    includeUnknowns: false,
+  } as AgeRestriction;
+
+  it('createAdmin() should create an admin user', () => {
+    const expectedRoles: Array<string> = ["Admin", "Login", "ChangePassword", "ChangeRestriction"]
+    const user: User = UserFactory.createAdmin();
+
+    expect(user.roles).toEqual(expectedRoles);
+    expect(user.username).toBeDefined();
+    expect(user.email).toBeDefined();
+    expect(user.apiKey).toBeDefined();
+    expect(user.scrobbleEventGenerationRan).toBeDefined();
+    expect(user.hasRunScrobbleEventGeneration).toBe(false);
+    expect(user.refreshToken).toBeDefined();
+    expect(user.preferences).toEqual(expectedPref);
+    expect(user.ageRestriction).toEqual(expectedAgeRestriction);
+  });
+
+  it('createUser() should create an regular user', () => {
+    const expectedRoles: Array<string> = ["Login", "ChangePassword"]
+    const user: User = UserFactory.createUser();
+
+    expect(user.roles).toEqual(expectedRoles);
+    expect(user.username).toBeDefined();
+    expect(user.email).toBeDefined();
+    expect(user.apiKey).toBeDefined();
+    expect(user.scrobbleEventGenerationRan).toBeDefined();
+    expect(user.hasRunScrobbleEventGeneration).toBe(false);
+    expect(user.refreshToken).toBeDefined();
+    expect(user.preferences).toEqual(expectedPref);
+    expect(user.ageRestriction).toEqual(expectedAgeRestriction);
+  });
+});
